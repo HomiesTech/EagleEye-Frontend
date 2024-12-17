eccda9356217c291227eed7bc76f24d985ad0681
YOGENDRA MISHRA <yogendramishrabetu@gmail.com>
2024-12-17 13:01:35 +0530

    showalertsbutton

--- src/component/Devices/DeviceInfoTable.tsx
+++ src/component/Devices/DeviceInfoTable.tsx
@@ -119,7 +119,7 @@ const DeviceInfoTable = ({ device }: { device: Device }) => {
               <button
                 onClick={handleShowAlarms}
                 className="bg-red-500 text-white px-4 py-2 rounded mt-2">
-                Alarm
+               Show
               </button>
             </td>
           </tbody>
