de1cfd548acaf35229a94bf309f3704fb8c17843
Abhismaxio <abhishek15mor@gmail.com>
2025-01-10 16:36:50 +0530

    change end points

--- src/config/constant.tsx
+++ src/config/constant.tsx
@@ -1,8 +1,8 @@
 const CONSTANTS ={
     version :"1.0.1",
     deviceVersionApi:"http://172.16.0.10:35000/list-versions",
-    binaryGeneratorApi:"http://192.168.29.49:35000/generate-binary",
-    restart:"http://172.236.179.216:35000/execute-command"
+    binaryGeneratorApi:"http://172.16.0.10:35000/generate-binary",
+    restart:"http://172.19.0.1:35000/execute-command"
 }
 // eslint-disable-next-line react-refresh/only-export-components
 export default CONSTANTS;
